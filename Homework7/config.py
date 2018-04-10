e988f4d70d8e1bda01e4f478a972efcb11b1f53f
Bhavani Yella <bhavaniyella@icloud.com>
2018-04-10 15:17:18 -0700

    1

--- Homework7/config.py
+++ Homework7/config.py
@@ -1,5 +1,5 @@
 # Twitter API Keys
-consumer_key = "Ed4RNulN1lp7AbOooHa9STCoU"
-consumer_secret = "P7cUJlmJZq0VaCY0Jg7COliwQqzK0qYEyUF9Y0idx4ujb3ZlW5"
-access_token = "839621358724198402-dzdOsx2WWHrSuBwyNUiqSEnTivHozAZ"
-access_token_secret = "dCZ80uNRbFDjxdU2EckmNiSckdoATach6Q8zb7YYYE5ER"
+consumer_key = "bKTlaC8L7zYVfowESkdtt8rP3"
+consumer_secret = "gdOTuyrDZuy5NkoEo5tNwcw0EOQDTV1ruIsC9xlWRtO25PI9z4"
+access_token = "983584039410937856-nnjmotaJ3LS9b167WoR9fAVgKSRbpqf"
+access_token_secret = "EF67djB5DwoAZ9Q5w8eTSTqeUZUOQBfuzSXlMgnnwcvfS"
